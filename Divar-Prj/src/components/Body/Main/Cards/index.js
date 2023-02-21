31bcf068cd6cbdf0256d9489251895afa8a3d170
Negar Sarani <negar.sarani2000@gmail.com>
2023-02-21 07:44:00 +0330

    create parent card

--- Divar-Prj/src/components/Body/Main/Cards/index.js
+++ Divar-Prj/src/components/Body/Main/Cards/index.js
@@ -3,13 +3,28 @@ import El from "../../../../library/El";
 const Cards = () => {
   return El({
     element: "div",
-    className: "bg-blue-400 lg:w-8/12 w-full ", //i change the w !!!
-    child: "hoo",
+    className: "w-full xl:pr-10 md:pr-5 ",
+    child: El({
+      element: "main",
+      className: "flex flex-col  w-full relative",
+      child: [
+        El({
+          element: "div",
+          className: "flex relative  w-full p-3 md:justify-end",
+          child: El({
+            element: "span",
+            className: " text-neutral-500 justify-center",
+            child: "دیوار تهران انولع آگهی‌ها و خدمات در تهران",
+          }),
+        }),
+        El({
+          element: "div",
+          className:
+            "grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4  w-full",
+            child:""
+        }),
+      ],
+    }),
   });
-  // El({
-  //     element:"div",
-  //     className:"bg-red-400",
-  //     child:"cardssssss",
-  // })
 };
 export default Cards;
---
+++ Divar-Prj/src/library/Cards/ElCard.js
@@ -0,0 +1,9 @@
+import El from "../El";
+
+const ElCard = () => {
+  return El({
+    element: "div",
+    className: "flex  rounded-sm shadow-sm border p-3 ",
+    child: "",
+  });
+};
